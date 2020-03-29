8d96b6fd6640e1ff63a0166e031504b0a202c0f7
wmc2tokyo <wmcooper2@yahoo.com>
2020-03-29 19:24:07 +0900

    Working on setting up email messages through contacts page. The hardest part has been understanding all the requirements for using SMTP and yahoo. There isn't enough help for my specific case, so I've been trying a lot of different things. I've been able to get it to mostly work, but there are still many issues.

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,12 +1,51 @@
 import React from "react";
 
-const Contact = props => {
+const EmailForm = () => {
   return (
-    <div className="topic-page">
+    <form
+      action="https://wmcooper2.com/tokyo-python/send"
+      method="post"
+      enctype="application/x-www-form-urlencoded"
+      // enctype="text/plain"
+    >
+      <input
+        type="text"
+        id="name"
+        name="sendersName"
+        placeholder="Name"
+        required
+      />
+      <input
+        type="email"
+        id="email"
+        name="sendersEmail"
+        placeholder="email"
+        required
+      />
+      <textarea
+        id="message"
+        name="message"
+        placeholder="Message... (1000 characters max)"
+        maxLength={1000}
+        wrap="hard"
+        required
+      />
+      <input type="submit" value="Submit" />
+    </form>
+  );
+};
+
+const ContactPage = props => {
+  return (
+    <div className="contact topic-page">
       <h1>Contact Page</h1>
-      <p>Form to submit email.</p>
+      <p>
+        Add something that spambots can't automatically input so that I know
+        you're not a robot spamming me. Captchas?
+      </p>
+      <EmailForm />
     </div>
   );
 };
 
-export default Contact;
+export default ContactPage;
